9304ca99f406f54427f11d7d554da2943ae6b027
usausa <machi.pon@gmail.com>
2024-04-13 21:26:14 +0900

    Update generator

--- MemberAccessorGenerator.SourceGenerator.Tests/MemberAccessorTest.cs
+++ MemberAccessorGenerator.SourceGenerator.Tests/MemberAccessorTest.cs
@@ -3,7 +3,31 @@ namespace MemberAccessorGenerator;
 public class MemberAccessorTest
 {
     [Fact]
-    public void Test1()
+    public void TestMemberAccessor()
     {
+        var accessorFactory = AccessorRegistry.FindFactory<Data>();
+
+        Assert.NotNull(accessorFactory);
+
+        var getId = accessorFactory.CreateGetter<int>(nameof(Data.Id));
+        var getName = accessorFactory.CreateGetter<string>(nameof(Data.Name));
+        var setId = accessorFactory.CreateSetter<int>(nameof(Data.Id));
+        var setName = accessorFactory.CreateSetter<string>(nameof(Data.Name));
+
+        Assert.NotNull(getId);
+        Assert.NotNull(getName);
+        Assert.NotNull(setId);
+        Assert.NotNull(setName);
+
+        var data = new Data { Id = 123, Name = "abc" };
+
+        Assert.Equal(123, getId(data));
+        Assert.Equal("abc", getName(data));
+
+        setId(data, 234);
+        setName(data, "xyz");
+
+        Assert.Equal(234, data.Id);
+        Assert.Equal("xyz", data.Name);
     }
 }
